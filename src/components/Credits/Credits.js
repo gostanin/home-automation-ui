274db5f680fa9aa8038963946c878043c1d42e7c
Gregory Ostanin <ostingreg@gmail.com>
2020-08-28 21:10:58 -0700

    open page update

--- src/components/Credits/Credits.js
+++ src/components/Credits/Credits.js
@@ -12,12 +12,12 @@ const Credits = (props) => {
             <div className={styles.credits}><Button color="primary" onClick={() => setOpen(true)}>Credits</Button></div>
             <Modal open={open} onClose={() => setOpen(false)} className={styles.modal}>
                 <div className={styles.paper}>
-                    <h4>Imagies provided by <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> authors:</h4>
+                    <h4>Imagies provided by <a href="https://www.flaticon.com/"  rel="noopener noreferrer" target="_blank" title="Flaticon">www.flaticon.com</a> authors:</h4>
                     <ul>
-                        <li><a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a></li>
-                        <li><a href="http://www.freepik.com/" title="Freepik">Freepik</a></li>
-                        <li><a href="https://www.flaticon.com/free-icon/thermometer_3030292" title="xnimrodx">xnimrodx</a></li>
-                        <li><a href="https://icon54.com/" title="Pixel perfect">Pixel perfect</a></li>
+                        <li><a href="https://www.flaticon.com/authors/good-ware" rel="noopener noreferrer" target="_blank" title="Good Ware">Good Ware</a></li>
+                        <li><a href="http://www.freepik.com/" rel="noopener noreferrer" target="_blank" title="Freepik">Freepik</a></li>
+                        <li><a href="https://www.flaticon.com/free-icon/thermometer_3030292" rel="noopener noreferrer" target="_blank" title="xnimrodx">xnimrodx</a></li>
+                        <li><a href="https://icon54.com/" rel="noopener noreferrer" target="_blank" title="Pixel perfect">Pixel perfect</a></li>
                     </ul>
                 </div>
             </Modal>
