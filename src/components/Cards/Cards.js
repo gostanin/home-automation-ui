dac45cff9794f84051e1a89cdbf3dcbb1ffb0578
Gregory Ostanin <ostingreg@gmail.com>
2020-08-28 18:16:37 -0700

    Sorting update

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Grid from '@material-ui/core/Grid';
 
@@ -8,20 +8,30 @@ import CardThermo from './Card/CardThermo/CardThermo';
 import CardAdd from './Card/CardAdd/CardAdd';
 
 const Cards = (props) => {
-    const lights = props.lights.map(light => {
-        return <CardLight key={light.id} id={light.id} name={light.name} status={light.status} deleteHandler={props.deleteHandler} />
-    })
+    const [devices, setDevices] = useState([]);
 
-    const thermo = props.thermo.map(thermo => {
-        return <CardThermo key={thermo.id} id={thermo.id} name={thermo.name} temp={thermo.temp} deleteHandler={props.deleteHandler} />
+    useEffect(() => {
+        const updatedLights = props.lights.map(light => ({ ...light, type: 'Light' }));
+        const updatedThermo = props.thermo.map(thermo => ({ ...thermo, type: 'Thermo' }));
+        const updatedDevices = [...updatedLights, ...updatedThermo].sort((a, b) => Date.parse(a.creation_date) - Date.parse(b.creation_date));
+        setDevices(updatedDevices);
+    }, [props]);
+
+    console.log('rendering');
+
+    const cards = devices.map(device => {
+        return (
+            device.type === 'Light'
+                ? <CardLight key={device.creation_date} id={device.id} name={device.name} status={device.status} deleteHandler={props.deleteHandler} />
+                : <CardThermo key={device.creation_date} id={device.id} name={device.name} temp={device.temp} deleteHandler={props.deleteHandler} />
+        );
     })
 
     return (
         <Grid className={styles.Cards}
             container>
             <CardAdd onClick={props.onOpen} />
-            {lights}
-            {thermo}
+            {cards}
         </Grid>
     );
 }
